5873ea07fff8c08546b718a1d4b348aa2fc0c038
James Laws <james@wpninjas.com>
2016-08-08 15:04:18 -0400

    CSS for conditionals and started adding an empty set of conditions for example.

--- assets/js/builder/views/advanced/conditionCollection.js
+++ assets/js/builder/views/advanced/conditionCollection.js
@@ -11,7 +11,20 @@ define( [ 'views/advanced/conditionItem' ], function( conditionItem ) {
 
 		initialize: function( options ) {
 			this.collection = options.dataModel.get( 'conditions' );
-		}
+		},
+
+        onRender: function() {
+            if ( 0 == this.collection.length ) {
+                this.collection.add();
+            }
+        },
+
+        onBeforeDestroy: function() {
+            if ( 0 == this.collection.length ) return;
+            if ( '' == this.collection.models[0].get('key') ) {
+                this.collection.reset();
+            }
+        }
 	});
 
 	return view;
