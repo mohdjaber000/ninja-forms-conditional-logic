9ff488793c2d6590f31ed79e2808aeffe7564243
Kyle B. Johnson <kyle@wpninjas.com>
2016-08-08 11:35:34 -0400

    bypass required validation for hidden fields.

--- includes/Triggers/HideField.php
+++ includes/Triggers/HideField.php
@@ -7,5 +7,12 @@ final class NF_ConditionalLogic_Triggers_HideField implements NF_ConditionalLogi
         $value = $field->get_setting( 'value' );
         $field->update_setting( 'value', false );
         $field->update_setting( 'submitted_value', $value );
+
+        // Hidden fields should NOT be validated for required.
+        if( 1 == $field->get_setting( 'required' ) ) {
+
+            // Set bypass flag.
+            $field->update_setting('conditionally_required', false);
+        }
     }
 }
