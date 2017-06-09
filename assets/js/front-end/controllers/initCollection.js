23f8fc9000c0c853e4ce4f717eaf2d9571b4dcf4
KR Moorhouse <spkr4thded@gmail.com>
2017-06-09 09:43:50 -0400

    Updated gitignore file to exclude mac os files. Added listener to collection init that will reset the collection to mirror a reset of the fields collection.

--- assets/js/front-end/controllers/initCollection.js
+++ assets/js/front-end/controllers/initCollection.js
@@ -8,8 +8,13 @@
 define( [ 'models/conditionCollection' ], function( ConditionCollection ) {
 	var controller = Marionette.Object.extend( {
 		initialize: function( formModel ) {
-			var collection = new ConditionCollection( formModel.get( 'conditions' ), { formModel: formModel } );
-		}
+			this.collection = new ConditionCollection( formModel.get( 'conditions' ), { formModel: formModel } );
+            this.listenTo(nfRadio.channel('fields'), 'reset:collection', this.resetCollection);
+		},
+        resetCollection: function( fieldsCollection ) {
+            var formModel = fieldsCollection.options.formModel;
+            this.collection = new ConditionCollection( formModel.get( 'conditions' ), { formModel: formModel } );
+        }
 	});
 
 	return controller;
