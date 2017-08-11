61220e98ca8a535de3b6964e6ede363495a7d836
Kenny Hall <klhall1987@gmail.com>
2017-08-11 11:33:08 -0400

    Added check to prevent the form from crashing on the front end if show/hide field was left blank. (#202)
    * Added check to prevent the form from crashing on the front end if show/hide field was left blank. closes #201

--- assets/js/front-end/controllers/showHide.js
+++ assets/js/front-end/controllers/showHide.js
@@ -27,6 +27,9 @@ define( [], function() {
 
 		showField: function( conditionModel, then ) {
 			var targetFieldModel = nfRadio.channel( 'form-' + conditionModel.collection.formModel.get( 'id' ) ).request( 'get:fieldByKey', then.key );
+
+			//TODO: Add an error to let the user know the show/hide field is empty.
+			if( 'undefined' == typeof targetFieldModel ) return;
 			targetFieldModel.set( 'visible', true );
 			if ( ! targetFieldModel.get( 'clean' ) ) {
 				targetFieldModel.trigger( 'change:value', targetFieldModel );
